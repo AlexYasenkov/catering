e3c1a07fb9b25046290138996386baa40d2cb6f1
Aleksandr <yasenkov.sasha@gmail.com>
2023-04-17 08:29:15 +0300

    Add Page 404

---
+++ wp-content/themes/food/404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<main class="page">
+    <section class="page-404">
+        <div class="container">
+            <h2 class="page-404__title">404 - Page Not Found</h2>
+        </div>
+    </section>
+</main>
+<?php get_footer(); ?>
